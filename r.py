c3702a427c30fefc7eac3510b7a4ae6f8bdebc72
Tejadeva123 <91255593+Tejadeva123@users.noreply.github.com>
2021-09-24 12:34:50 +0530

    Add files via upload

---
+++ r.py
@@ -0,0 +1,6 @@
+a=10
+b=9
+if a>b:
+    print("False")
+else a==b:
+    print("True")
---
+++ rfeg.py
@@ -0,0 +1,17 @@
+a=64
+b=73
+if a>b:
+    print("True")
+else:
+    print("False")
+a=("hellow,wpeld")
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace("p","o"))
+print(a.split(" ,"))
+a=64
+b=73
+c=a+b
+print(c)
