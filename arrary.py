3607bd9811658b135dfe10eee6d356bebd108447
Tejadeva123 <91255593+Tejadeva123@users.noreply.github.com>
2021-09-29 14:35:44 +0530

    Add files via upload

---
+++ arrary.py
@@ -0,0 +1,9 @@
+x=["apple","ant","banana","cherry"]
+print(x)
+print(x[0])
+x[0]="njhjsn"
+print(len(x))
+x.append("hbdh")
+x.remove("banana")
+for i in x:
+    print(i)
---
+++ function.py
@@ -0,0 +1,8 @@
+x=lambda a : a+24
+print(x(8))
+x=lambda a ,b : a*b
+print(x(6,4))
+x=lambda a,b,c :a+b+c
+print(x(3,5,6))
+x=lambda a,b,c : (a+b)*c
+print(x(6,7,8))
